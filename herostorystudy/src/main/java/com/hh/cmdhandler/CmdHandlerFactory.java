4d23c73f8defd472d29b9168d35fcab76a3e5f15
huanghong01 <huanghong01@cnpc.com.cn>
2019-12-17 11:29:17 +0800

    重构CmdHandlerFactory

--- herostorystudy/src/main/java/com/hh/cmdhandler/CmdHandlerFactory.java
+++ herostorystudy/src/main/java/com/hh/cmdhandler/CmdHandlerFactory.java
@@ -2,16 +2,47 @@ package com.hh.cmdhandler;
 
 import com.google.protobuf.GeneratedMessageV3;
 import com.hh.msg.GameMsgProtocol;
+import com.hh.util.PackageUtil;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class CmdHandlerFactory {
     private static final Map<Class<?> ,CmdHandler<? extends GeneratedMessageV3>> CMD_HANDLER_MAP = new HashMap<>();
 
     static {
-        CMD_HANDLER_MAP.put(GameMsgProtocol.UserEntryCmd.class,new UserEntryCmdHandler());
-        CMD_HANDLER_MAP.put(GameMsgProtocol.WhoElseIsHereCmd.class,new WhoElseIsHereCmdHandler());
+//        CMD_HANDLER_MAP.put(GameMsgProtocol.UserEntryCmd.class,new UserEntryCmdHandler());
+//        CMD_HANDLER_MAP.put(GameMsgProtocol.WhoElseIsHereCmd.class,new WhoElseIsHereCmdHandler());
+        String pakageName = CmdHandlerFactory.class.getPackage().getName();
+        Set<Class<?>> listSubClazz = PackageUtil.listSubClazz(pakageName, true, CmdHandler.class);
+        for (Class<?> subClazz : listSubClazz) {
+            if((subClazz.getModifiers() & Modifier.ABSTRACT) != 0){
+                continue;
+            }
+
+            Method[] methods = subClazz.getDeclaredMethods();
+
+            for (Method method : methods) {
+                if(!(method.getName().equals("handle"))){
+                    continue;
+                }
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                if(parameterTypes.length != 2){
+                    continue;
+                }
+                try {
+                    CmdHandler<?> handler = (CmdHandler) subClazz.newInstance();
+                    CMD_HANDLER_MAP.put(parameterTypes[1],handler);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
